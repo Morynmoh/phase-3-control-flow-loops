5f2bb7ff870b3e6a9abe031951f0502ae817da96
Maureen <morynmoh@gmail.com>
2023-06-28 10:52:56 +0300

    Assignment done

--- looping.rb
+++ looping.rb
@@ -1,6 +1,12 @@
 def happy_new_year
-  # your code here
+  i = 10
+  while i > 0
+    puts "#{i}"
+    i -= 1 #decrement in Ruby i--
+  end
+  puts "Happy New Year!"
 end
+happy_new_year
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -17,8 +23,37 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
+
 end
+
+puts fizzbuzz_printer
 
 def reverse_string(str)
   # your code here
+  # str.reverse
+  #convert string to array using chars method
+  characters = str.chars
+  reversed_characters = []
+   # Iterate over the characters array in reverse order and add them to the reversed_characters array
+   (characters.length - 1).downto(0) do |i|
+    reversed_characters << characters[i]
+  end
+  
+  reversed_string = reversed_characters.join('')  # Convert the reversed array back to a string
+  
+  return reversed_string
+
 end
+puts reverse_string("hello")
+
